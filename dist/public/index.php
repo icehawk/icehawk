c75e0826c8724d2903df47718f83f96f3e16556a
Holger Woltersdorf <hw@hollo.me>
2015-02-03 12:07:02 +0100

    Added example index.php

--- dist/public/index.php
+++ dist/public/index.php
@@ -7,16 +7,11 @@ namespace Fortuneglobe\IceHawk;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use Fortuneglobe\IceHawk\Responses\Page;
-
 error_reporting( E_ALL );
 ini_set( 'display_errors', 1 );
 
-$icehawk = IceHawk::fromSky();
+$icehawk = IceHawk::fromSky( new IceHawkDelegate() );
 $icehawk->init();
 
 $request_handler = $icehawk->getRequestHandler();
 $request_handler->handle();
-
-$page = new Page( 'Test.twig', [ 'world' => 'Welt' ] );
-$page->respond();

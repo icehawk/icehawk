8dc398289bff459e90722e8f2ebb31a238d9129b
Holger Woltersdorf <h.woltersdorf@fortuneglobe.com>
2015-02-05 16:16:27 +0100

    Added base method to rewrite by a simple map

--- dist/UriRewriter.php
+++ dist/UriRewriter.php
@@ -24,4 +24,48 @@ class UriRewriter implements RewritesUri
 	{
 		return $request_info->getUri();
 	}
+
+	/**
+	 * @param string $request_uri
+	 * @param array  $simple_map
+	 *
+	 * @return string
+	 */
+	protected function rewriteUriBySimpleMap( $request_uri, array $simple_map )
+	{
+		$mapped_uri = $request_uri;
+
+		foreach ( $simple_map as $pattern => $uri )
+		{
+			if ( $this->uriMatchesPattern( $request_uri, $pattern ) )
+			{
+				$mapped_uri = $uri;
+				break;
+			}
+		}
+
+		return $mapped_uri;
+	}
+
+	/**
+	 * @param string $request_uri
+	 * @param string $pattern
+	 *
+	 * @return bool
+	 */
+	private function uriMatchesPattern( $request_uri, $pattern )
+	{
+		if ( $request_uri == $pattern )
+		{
+			return true;
+		}
+		elseif ( @preg_match( $pattern, '' ) !== false )
+		{
+			return boolval( preg_match( $pattern, $request_uri ) );
+		}
+		else
+		{
+			return false;
+		}
+	}
 }

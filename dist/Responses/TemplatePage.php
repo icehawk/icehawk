b35291846d18fe976c438a23310b25ea6ec420a9
Holger Woltersdorf <h.woltersdorf@fortuneglobe.com>
2015-02-13 15:25:35 +0100

    Removed singleton call from template page removed getTemplateEngine from delegate interface

---
+++ dist/Responses/TemplatePage.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @author hollodotme
+ */
+
+namespace Fortuneglobe\IceHawk\Responses;
+
+use Fortuneglobe\IceHawk\Interfaces\RendersTemplate;
+
+class TemplatePage extends BaseResponse
+{
+
+	/** @var string */
+	private $template;
+
+	/** @var array */
+	private $data;
+
+	/** @var string */
+	private $charset;
+
+	/** @var RendersTemplate */
+	private $templateEngine;
+
+	/**
+	 * @param string          $template
+	 * @param array           $data
+	 * @param string          $charset
+	 * @param RendersTemplate $templateEngine
+	 */
+	public function __construct( $template, array $data, $charset = 'utf-8', RendersTemplate $templateEngine )
+	{
+		$this->template       = $template;
+		$this->data           = $data;
+		$this->charset        = $charset;
+		$this->templateEngine = $templateEngine;
+	}
+
+	public function respond()
+	{
+		header( 'Content-Type: text/html; charset=' . $this->charset, true );
+		echo $this->templateEngine->renderWithData( $this->template, $this->data );
+	}
+}
---
+++ test/Unit/Responses/TemplatePageTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author h.woltersdorf
+ */
+
+namespace Fortuneglobe\IceHawk\Test\Unit\Responses;
+
+use Fortuneglobe\IceHawk\Interfaces\RendersTemplate;
+use Fortuneglobe\IceHawk\Responses\TemplatePage;
+
+class TemplatePageTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @runInSeparateProcess
+	 */
+	public function testRespondsHtmlWithData()
+	{
+		$data = [ 'unit', 'test' ];
+
+		$templateEngine = $this->getMockBuilder( RendersTemplate::class )
+		                       ->setMethods( [ 'renderWithData' ] )
+		                       ->getMock();
+
+		$templateEngine->expects( $this->once() )
+		               ->method( 'renderWithData' )
+		               ->willReturnCallback(
+			               function ( $template, array $data )
+			               {
+				               return $template . ' - ' . join( "\n", $data );
+			               }
+		               );
+
+		( new TemplatePage( '/Unit/Test.tpl', $data, 'utf-8', $templateEngine ) )->respond();
+
+		$this->assertContains( 'Content-Type: text/html; charset=utf-8', xdebug_get_headers() );
+		$this->expectOutputString( "/Unit/Test.tpl - unit\ntest" );
+	}
+}

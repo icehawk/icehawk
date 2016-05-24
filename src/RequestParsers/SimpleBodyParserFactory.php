0a00d1e4d499293d0fa23e96039d1b658a2efea3
Benjamin Bruska <benjamin.bruska@fortuneglobe.com>
2016-05-24 09:12:10 +0200

    AbstractBodyParserFactory is already checking if there is a body-parser for given content-type

--- src/RequestParsers/SimpleBodyParserFactory.php
+++ src/RequestParsers/SimpleBodyParserFactory.php
@@ -1,7 +1,6 @@
 <?php
 namespace Fortuneglobe\IceHawk\RequestParsers;
 
-use Fortuneglobe\IceHawk\Exceptions\MissingBodyContentParser;
 use Fortuneglobe\IceHawk\Interfaces\ParsesRequestBody;
 
 /**
@@ -13,12 +12,7 @@ class SimpleBodyParserFactory extends AbstractBodyParserFactory
 {
 	protected function createParserByContentType( string $contentType ) : ParsesRequestBody
 	{
-		if ( empty($contentType) || $contentType == 'application/x-www-form-urlencoded' )
-		{
-			return new FormBodyParser();
-		}
-
-		throw ( new MissingBodyContentParser() )->withContentType( $contentType );
+		return new FormBodyParser();
 	}
 
 	protected function canCreateParserForContentType( string $contentType ) : bool

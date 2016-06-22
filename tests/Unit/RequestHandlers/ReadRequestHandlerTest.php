31b8c2843a58224eaeb836bd40b0b61073adb4a3
Benjamin Bruska <benjamin.bruska@fortuneglobe.com>
2016-06-22 17:36:25 +0200

    adapt tests

--- tests/Unit/RequestHandlers/ReadRequestHandlerTest.php
+++ tests/Unit/RequestHandlers/ReadRequestHandlerTest.php
@@ -7,8 +7,11 @@ namespace Fortuneglobe\IceHawk\Tests\Unit\RequestHandlers;
 
 use Fortuneglobe\IceHawk\Defaults\RequestInfo;
 use Fortuneglobe\IceHawk\Interfaces\ConfiguresIceHawk;
+use Fortuneglobe\IceHawk\Interfaces\HandlesGetRequest;
+use Fortuneglobe\IceHawk\Interfaces\RespondsFinallyToReadRequest;
 use Fortuneglobe\IceHawk\PubSub\EventPublisher;
 use Fortuneglobe\IceHawk\RequestHandlers\ReadRequestHandler;
+use Fortuneglobe\IceHawk\Routing\Patterns\Literal;
 use Fortuneglobe\IceHawk\Routing\Patterns\RegExp;
 use Fortuneglobe\IceHawk\Routing\ReadRoute;
 use Fortuneglobe\IceHawk\Tests\Unit\Fixtures\Domain\Read\RequestParamsRequestHandler;
@@ -49,7 +52,7 @@ class ReadRequestHandlerTest extends \PHPUnit_Framework_TestCase
 		$requestInfo = new RequestInfo( [ 'REQUEST_METHOD' => 'GET', 'REQUEST_URI' => $requestUri ] );
 
 		$regExp    = new RegExp( sprintf( '#^/domain/test_request_param/%s/(%s)$#', $uriKey, $uriValue ), [ $uriKey ] );
-		$readRoute = new ReadRoute(	$regExp, new RequestParamsRequestHandler() );
+		$readRoute = new ReadRoute( $regExp, new RequestParamsRequestHandler() );
 
 		$config->method( 'getRequestInfo' )->willReturn( $requestInfo );
 
@@ -59,5 +62,74 @@ class ReadRequestHandlerTest extends \PHPUnit_Framework_TestCase
 		$readRequestHandler->handleRequest();
 
 		$this->expectOutputString( $expectedJson );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 */
+	public function testMissingReadRoutesHandledByFinaleReadResponder()
+	{
+		$requestInfo = new RequestInfo( [ 'REQUEST_METHOD' => 'GET', 'REQUEST_URI' => '/test' ] );
+
+		$finalReadResponder = $this->getMockBuilder( RespondsFinallyToReadRequest::class )->getMockForAbstractClass();
+		$finalReadResponder->method( 'handleUncaughtException' )
+		                   ->will(
+			                   $this->returnCallback(
+				                   function ()
+				                   {
+					                   echo 'fine';
+				                   }
+			                   )
+		                   );
+		$config = $this->getMockBuilder( ConfiguresIceHawk::class )->getMockForAbstractClass();
+
+		$config->method( 'getRequestInfo' )->willReturn( $requestInfo );
+		$config->expects( $this->once() )->method( 'getReadRoutes' )->willReturn( [ ] );
+		$config->method( 'getFinalReadResponder' )->willReturn( $finalReadResponder );
+
+		$readRequestHandler = new ReadRequestHandler( $config, new EventPublisher() );
+		$readRequestHandler->handleRequest();
+
+		$this->expectOutputString( 'fine' );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 */
+	public function testExceptionHandledByFinaleReadResponder()
+	{
+		$requestInfo = new RequestInfo( [ 'REQUEST_METHOD' => 'GET', 'REQUEST_URI' => '/test' ] );
+
+		$exception = new \Exception();
+
+		$finalReadResponder = $this->getMockBuilder( RespondsFinallyToReadRequest::class )->getMockForAbstractClass();
+		$finalReadResponder->method( 'handleUncaughtException' )
+		                   ->will(
+			                   $this->returnCallback(
+				                   function ( $exception )
+				                   {
+					                   echo get_class( $exception );
+				                   }
+			                   )
+		                   );
+
+		$config = $this->getMockBuilder( ConfiguresIceHawk::class )->getMockForAbstractClass();
+
+		$config->method( 'getRequestInfo' )->willReturn( $requestInfo );
+		$config->method( 'getFinalReadResponder' )->willReturn( $finalReadResponder );
+
+		$requestHandler = $this->getMockBuilder( HandlesGetRequest::class )->getMockForAbstractClass();
+		$requestHandler->expects( $this->once() )
+		               ->method( 'handle' )
+		               ->will( $this->throwException( $exception ) );
+
+		$readRoute = new ReadRoute( new Literal( '/test' ), $requestHandler );
+
+		$config->expects( $this->once() )->method( 'getReadRoutes' )->willReturn( [ $readRoute ] );
+
+		$readRequestHandler = new ReadRequestHandler( $config, new EventPublisher() );
+		$readRequestHandler->handleRequest();
+
+		$this->expectOutputString( get_class( $exception ) );
 	}
 }
--- tests/Unit/RequestHandlers/WriteRequestHandlerTest.php
+++ tests/Unit/RequestHandlers/WriteRequestHandlerTest.php
@@ -7,6 +7,8 @@ namespace Fortuneglobe\IceHawk\Tests\Unit\RequestHandlers;
 
 use Fortuneglobe\IceHawk\Defaults\RequestInfo;
 use Fortuneglobe\IceHawk\Interfaces\ConfiguresIceHawk;
+use Fortuneglobe\IceHawk\Interfaces\HandlesPostRequest;
+use Fortuneglobe\IceHawk\Interfaces\RespondsFinallyToWriteRequest;
 use Fortuneglobe\IceHawk\PubSub\EventPublisher;
 use Fortuneglobe\IceHawk\RequestHandlers\WriteRequestHandler;
 use Fortuneglobe\IceHawk\Routing\Patterns\Literal;
@@ -24,17 +26,17 @@ class WriteRequestHandlerTest extends \PHPUnit_Framework_TestCase
 			[
 				[ 'unit' => 'test', 'test' => 'unit' ],
 				'unit', 'tested',
-				json_encode( [ 'unit' => 'tested', 'test' => 'unit' ] )
+				json_encode( [ 'unit' => 'tested', 'test' => 'unit' ] ),
 			],
 			[
 				[ 'unit' => 'test', 'test' => 'unit' ],
 				'test', 'units',
-				json_encode( [ 'unit' => 'test', 'test' => 'units' ] )
+				json_encode( [ 'unit' => 'test', 'test' => 'units' ] ),
 			],
 			[
 				[ 'unit' => [ 'test' => 'unit' ] ],
 				'unit', 'units',
-				json_encode( [ 'unit' => 'units' ] )
+				json_encode( [ 'unit' => 'units' ] ),
 			],
 		];
 	}
@@ -43,18 +45,21 @@ class WriteRequestHandlerTest extends \PHPUnit_Framework_TestCase
 	 * @dataProvider parameterProvider
 	 * @runInSeparateProcess
 	 */
-	public function testUriParamsOverwritesPostParams( array $postData, string $uriKey, string $uriValue, $expectedJson )
+	public function testUriParamsOverwritesPostParams( array $postData, string $uriKey, string $uriValue, $expectedJson
+	)
 	{
 		$_POST = $postData;
 
 		$config      = $this->getMockBuilder( ConfiguresIceHawk::class )->getMockForAbstractClass();
-		$requestInfo = new RequestInfo( 
-			[ 'REQUEST_METHOD' => 'POST',
-		      'REQUEST_URI' => sprintf( '/domain/test_request_param/%s/%s', $uriKey, $uriValue ) ] 
+		$requestInfo = new RequestInfo(
+			[
+				'REQUEST_METHOD' => 'POST',
+				'REQUEST_URI'    => sprintf( '/domain/test_request_param/%s/%s', $uriKey, $uriValue ),
+			]
 		);
 
-		$regExp     = new RegExp( 
-			sprintf( '#^/domain/test_request_param/%s/(%s)$#', $uriKey, $uriValue ), [ $uriKey ] 
+		$regExp     = new RegExp(
+			sprintf( '#^/domain/test_request_param/%s/(%s)$#', $uriKey, $uriValue ), [ $uriKey ]
 		);
 		$writeRoute = new WriteRoute( $regExp, new RequestParamsRequestHandler() );
 
@@ -70,7 +75,7 @@ class WriteRequestHandlerTest extends \PHPUnit_Framework_TestCase
 	/**
 	 * @runInSeparateProcess
 	 */
-	public function testCanGetBodyDataFromInputStream( )
+	public function testCanGetBodyDataFromInputStream()
 	{
 		stream_wrapper_unregister( "php" );
 		stream_wrapper_register( "php", PhpStreamMock::class );
@@ -80,7 +85,7 @@ class WriteRequestHandlerTest extends \PHPUnit_Framework_TestCase
 		$requestInfo = new RequestInfo( [ 'REQUEST_METHOD' => 'POST', 'REQUEST_URI' => '/domain/test_body_data' ] );
 
 		$writeRoute = new WriteRoute( new Literal( '/domain/test_body_data' ), new BodyDataRequestHandler() );
-		
+
 		$config->method( 'getRequestInfo' )->willReturn( $requestInfo );
 		$config->expects( $this->once() )->method( 'getWriteRoutes' )->willReturn( [ $writeRoute ] );
 
@@ -90,5 +95,74 @@ class WriteRequestHandlerTest extends \PHPUnit_Framework_TestCase
 		$this->expectOutputString( 'body data' );
 
 		stream_wrapper_restore( "php" );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 */
+	public function testMissingWriteRoutesHandledByFinaleWriteResponder()
+	{
+		$requestInfo = new RequestInfo( [ 'REQUEST_METHOD' => 'GET', 'REQUEST_URI' => '/test' ] );
+
+		$finalWriteResponder = $this->getMockBuilder( RespondsFinallyToWriteRequest::class )->getMockForAbstractClass();
+		$finalWriteResponder->method( 'handleUncaughtException' )
+		                    ->will(
+			                    $this->returnCallback(
+				                    function ()
+				                    {
+					                    echo 'fine';
+				                    }
+			                    )
+		                    );
+
+		$config = $this->getMockBuilder( ConfiguresIceHawk::class )->getMockForAbstractClass();
+
+		$config->method( 'getRequestInfo' )->willReturn( $requestInfo );
+		$config->expects( $this->once() )->method( 'getWriteRoutes' )->willReturn( [ ] );
+		$config->method( 'getFinalWriteResponder' )->willReturn( $finalWriteResponder );
+
+		$writeRequestHandler = new WriteRequestHandler( $config, new EventPublisher() );
+		$writeRequestHandler->handleRequest();
+
+		$this->expectOutputString( 'fine' );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 */
+	public function testExceptionHandledByFinaleWriteResponder()
+	{
+		$requestInfo = new RequestInfo( [ 'REQUEST_METHOD' => 'POST', 'REQUEST_URI' => '/test' ] );
+		$exception   = new \Exception();
+
+		$finalWriteResponder = $this->getMockBuilder( RespondsFinallyToWriteRequest::class )->getMockForAbstractClass();
+		$finalWriteResponder->method( 'handleUncaughtException' )
+		                    ->will(
+			                    $this->returnCallback(
+				                    function ( $exception )
+				                    {
+					                    echo get_class( $exception );
+				                    }
+			                    )
+		                    );
+
+		$config = $this->getMockBuilder( ConfiguresIceHawk::class )->getMockForAbstractClass();
+		
+		$config->method( 'getRequestInfo' )->willReturn( $requestInfo );
+		$config->method( 'getFinalWriteResponder' )->willReturn( $finalWriteResponder );
+
+		$requestHandler = $this->getMockBuilder( HandlesPostRequest::class )->getMockForAbstractClass();
+		$requestHandler->expects( $this->once() )
+		               ->method( 'handle' )
+		               ->will( $this->throwException( $exception ) );
+
+		$writeRoute = new WriteRoute( new Literal( '/test' ), $requestHandler );
+
+		$config->expects( $this->once() )->method( 'getWriteRoutes' )->willReturn( [ $writeRoute ] );
+
+		$writeRequestHandler = new WriteRequestHandler( $config, new EventPublisher() );
+		$writeRequestHandler->handleRequest();
+
+		$this->expectOutputString( get_class( $exception ) );
 	}
 }

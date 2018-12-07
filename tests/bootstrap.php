df5d8caae9907cdb903448d7f8809f2162f0af71
Holger Woltersdorf <hw@hollo.me>
2018-12-07 21:00:27 +0100

    Add Xdebug coverage path whitelisting filter

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,17 @@
 <?php declare(strict_types=1);
-/**
- * Copyright (c) 2017 Holger Woltersdorf & Contributors
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- */
+
+if (
+	extension_loaded( 'xdebug' )
+	&& version_compare( '2.6.0', phpversion( 'xdebug' ), '<=' )
+)
+{
+	/** @noinspection PhpUndefinedFunctionInspection */
+	/** @noinspection PhpUndefinedConstantInspection */
+	xdebug_set_filter(
+		XDEBUG_FILTER_CODE_COVERAGE,
+		XDEBUG_PATH_WHITELIST,
+		[dirname( __DIR__ ) . '/src']
+	);
+}
 
 require_once __DIR__ . '/../vendor/autoload.php';

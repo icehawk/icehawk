038bc73d52573a36d94fcfc71090f5630866cfad
Pavel Bisse <pavel.bisse@fortuneglobe.com>
2019-08-28 16:06:10 +0200

    fix namespace

--- tests/Unit/Messages/UploadedFileTest.php
+++ tests/Unit/Messages/UploadedFileTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Fortuneglobe\IceLynx\Server\Tests\Unit;
+namespace IceHawk\IceHawk\Tests\Unit\Messages;
 
 use IceHawk\IceHawk\Exceptions\InvalidArgumentException;
 use PHPUnit\Framework\TestCase;

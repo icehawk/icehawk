0faa0557e32876739539585894e09e653dea752a
Holger Woltersdorf <hw@hollo.me>
2020-04-05 22:44:41 +0200

    Change bootstrap to composer autoload file, add Xdebug whitelist filter

---
+++ build/xdebug_filter.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+if ( !function_exists( 'xdebug_set_filter' ) )
+{
+	return;
+}
+
+/** @noinspection PhpUndefinedConstantInspection */
+xdebug_set_filter(
+	XDEBUG_FILTER_CODE_COVERAGE,
+	XDEBUG_PATH_WHITELIST,
+	[dirname( __DIR__ ) . '/src']
+);

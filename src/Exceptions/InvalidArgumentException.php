6387e85c5ac8fa1594f42caf8dcb4faa933aedb0
Holger Woltersdorf <hw@hollo.me>
2020-04-05 22:45:31 +0200

    Remove package exceptions

--- src/Exceptions/InvalidArgumentException.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace IceHawk\IceHawk\Exceptions;
-
-class InvalidArgumentException extends \InvalidArgumentException
-{
-
-}
--- src/Exceptions/RuntimeException.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace IceHawk\IceHawk\Exceptions;
-
-class RuntimeException extends \RuntimeException
-{
-
-}

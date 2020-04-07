1e10cd919c773fe217519c67544d527b60e5bc0f
Holger Woltersdorf <hw@hollo.me>
2020-04-07 12:08:00 +0200

    Add nested files array provider

--- tests/Fixtures/Traits/UploadedFilesProviding.php
+++ tests/Fixtures/Traits/UploadedFilesProviding.php
@@ -3,25 +3,40 @@
 namespace IceHawk\IceHawk\Tests\Fixtures\Traits;
 
 use IceHawk\IceHawk\Messages\UploadedFile;
+use const UPLOAD_ERR_NO_FILE;
+use const UPLOAD_ERR_OK;
 
 trait UploadedFilesProviding
 {
 	private function filesArray() : array
 	{
 		return [
-			[
+			'file1' => [
 				'name'     => 'test.txt',
 				'type'     => 'text/plain',
 				'tmp_name' => '/tmp/php/efgwef98',
 				'error'    => UPLOAD_ERR_OK,
 				'size'     => 563,
 			],
-			[
+			'file2' => [
 				'name'     => 'test.txt',
 				'type'     => 'text/plain',
 				'tmp_name' => '/tmp/php/khjgjkhg43',
 				'error'    => UPLOAD_ERR_NO_FILE,
 				'size'     => 0,
+			],
+		];
+	}
+
+	private function nestedFilesArray() : array
+	{
+		return [
+			'files' => [
+				'name'     => ['test1.txt', 'test2.json'],
+				'type'     => ['text/plain', 'application/json'],
+				'tmp_name' => ['/tmp/php/efgwef98', '/tmp/php/khjgjkhg43'],
+				'error'    => [UPLOAD_ERR_OK, UPLOAD_ERR_NO_FILE],
+				'size'     => [563, 0],
 			],
 		];
 	}

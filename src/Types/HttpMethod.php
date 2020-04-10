f0840321b6ab865c5912687e63ed38507067e59a
Holger Woltersdorf <hw@hollo.me>
2020-04-10 10:13:22 +0200

    Add HTTP method type incl. tests

---
+++ src/Types/HttpMethod.php
@@ -0,0 +1,168 @@
+<?php declare(strict_types=1);
+
+namespace IceHawk\IceHawk\Types;
+
+use InvalidArgumentException;
+use function strtoupper;
+
+final class HttpMethod
+{
+	private const GET     = 'GET';
+
+	private const HEAD    = 'HEAD';
+
+	private const POST    = 'POST';
+
+	private const PUT     = 'PUT';
+
+	private const PATCH   = 'PATCH';
+
+	private const DELETE  = 'DELETE';
+
+	private const OPTIONS = 'OPTIONS';
+
+	private const CONNECT = 'CONNECT';
+
+	private const TRACE   = 'TRACE';
+
+	private const ALL     = [
+		self::GET,
+		self::HEAD,
+		self::POST,
+		self::PUT,
+		self::PATCH,
+		self::DELETE,
+		self::OPTIONS,
+		self::CONNECT,
+		self::TRACE,
+	];
+
+	private string $httpMethod;
+
+	/**
+	 * @param string $httpMethod
+	 *
+	 * @throws InvalidArgumentException
+	 */
+	private function __construct( string $httpMethod )
+	{
+		$this->guardHttpMethodIsValid( $httpMethod );
+
+		$this->httpMethod = strtoupper( $httpMethod );
+	}
+
+	/**
+	 * @param string $httpMethod
+	 *
+	 * @throws InvalidArgumentException
+	 */
+	private function guardHttpMethodIsValid( string $httpMethod ) : void
+	{
+		if ( !in_array( strtoupper( $httpMethod ), self::ALL, true ) )
+		{
+			throw new InvalidArgumentException( 'Invalid value for HttpMethod: ' . $httpMethod );
+		}
+	}
+
+	/**
+	 * @param string $httpMethod
+	 *
+	 * @return HttpMethod
+	 * @throws InvalidArgumentException
+	 */
+	public static function newFromString( string $httpMethod ) : self
+	{
+		return new self( $httpMethod );
+	}
+
+	/**
+	 * @return HttpMethod
+	 * @throws InvalidArgumentException
+	 */
+	public static function get() : self
+	{
+		return new self( self::GET );
+	}
+
+	/**
+	 * @return HttpMethod
+	 * @throws InvalidArgumentException
+	 */
+	public static function head() : self
+	{
+		return new self( self::HEAD );
+	}
+
+	/**
+	 * @return HttpMethod
+	 * @throws InvalidArgumentException
+	 */
+	public static function post() : self
+	{
+		return new self( self::POST );
+	}
+
+	/**
+	 * @return HttpMethod
+	 * @throws InvalidArgumentException
+	 */
+	public static function put() : self
+	{
+		return new self( self::PUT );
+	}
+
+	/**
+	 * @return HttpMethod
+	 * @throws InvalidArgumentException
+	 */
+	public static function patch() : self
+	{
+		return new self( self::PATCH );
+	}
+
+	/**
+	 * @return HttpMethod
+	 * @throws InvalidArgumentException
+	 */
+	public static function delete() : self
+	{
+		return new self( self::DELETE );
+	}
+
+	/**
+	 * @return HttpMethod
+	 * @throws InvalidArgumentException
+	 */
+	public static function options() : self
+	{
+		return new self( self::OPTIONS );
+	}
+
+	/**
+	 * @return HttpMethod
+	 * @throws InvalidArgumentException
+	 */
+	public static function connect() : self
+	{
+		return new self( self::CONNECT );
+	}
+
+	/**
+	 * @return HttpMethod
+	 * @throws InvalidArgumentException
+	 */
+	public static function trace() : self
+	{
+		return new self( self::TRACE );
+	}
+
+	public function toString() : string
+	{
+		return $this->httpMethod;
+	}
+
+	public function equals( HttpMethod $other ) : bool
+	{
+		return $other->httpMethod === $this->httpMethod;
+	}
+}
---
+++ tests/Unit/Types/HttpMethodTest.php
@@ -0,0 +1,135 @@
+<?php declare(strict_types=1);
+
+namespace IceHawk\IceHawk\Tests\Unit\Types;
+
+use IceHawk\IceHawk\Types\HttpMethod;
+use InvalidArgumentException;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+
+final class HttpMethodTest extends TestCase
+{
+	/**
+	 * @throws InvalidArgumentException
+	 * @throws ExpectationFailedException
+	 */
+	public function testHead() : void
+	{
+		$this->assertSame( 'HEAD', HttpMethod::head()->toString() );
+	}
+
+	/**
+	 * @throws InvalidArgumentException
+	 * @throws ExpectationFailedException
+	 */
+	public function testPut() : void
+	{
+		$this->assertSame( 'PUT', HttpMethod::put()->toString() );
+	}
+
+	/**
+	 * @throws ExpectationFailedException
+	 * @throws InvalidArgumentException
+	 */
+	public function testTrace() : void
+	{
+		$this->assertSame( 'TRACE', HttpMethod::trace()->toString() );
+	}
+
+	/**
+	 * @throws ExpectationFailedException
+	 * @throws InvalidArgumentException
+	 */
+	public function testToString() : void
+	{
+		$this->assertSame( 'HEAD', HttpMethod::newFromString( 'head' )->toString() );
+		$this->assertSame( 'GET', HttpMethod::newFromString( 'Get' )->toString() );
+		$this->assertSame( 'POST', HttpMethod::newFromString( 'poST' )->toString() );
+	}
+
+	/**
+	 * @throws ExpectationFailedException
+	 * @throws InvalidArgumentException
+	 */
+	public function testOptions() : void
+	{
+		$this->assertSame( 'OPTIONS', HttpMethod::options()->toString() );
+	}
+
+	/**
+	 * @throws ExpectationFailedException
+	 * @throws InvalidArgumentException
+	 */
+	public function testPatch() : void
+	{
+		$this->assertSame( 'PATCH', HttpMethod::patch()->toString() );
+	}
+
+	/**
+	 * @throws ExpectationFailedException
+	 * @throws InvalidArgumentException
+	 */
+	public function testConnect() : void
+	{
+		$this->assertSame( 'CONNECT', HttpMethod::connect()->toString() );
+	}
+
+	/**
+	 * @throws ExpectationFailedException
+	 * @throws InvalidArgumentException
+	 */
+	public function testNewFromString() : void
+	{
+		$this->assertSame( 'HEAD', HttpMethod::newFromString( 'head' )->toString() );
+		$this->assertSame( 'GET', HttpMethod::newFromString( 'Get' )->toString() );
+		$this->assertSame( 'POST', HttpMethod::newFromString( 'poST' )->toString() );
+	}
+
+	/**
+	 * @throws InvalidArgumentException
+	 */
+	public function testNewFromStringThrowsExceptionForInvalidHttpMethod() : void
+	{
+		$this->expectException( InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'Invalid value for HttpMethod: unknown' );
+
+		HttpMethod::newFromString( 'unknown' );
+	}
+
+	/**
+	 * @throws ExpectationFailedException
+	 * @throws InvalidArgumentException
+	 */
+	public function testDelete() : void
+	{
+		$this->assertSame( 'DELETE', HttpMethod::delete()->toString() );
+	}
+
+	/**
+	 * @throws ExpectationFailedException
+	 * @throws InvalidArgumentException
+	 */
+	public function testEquals() : void
+	{
+		$this->assertTrue( HttpMethod::delete()->equals( HttpMethod::newFromString( 'delete' ) ) );
+		$this->assertFalse( HttpMethod::delete()->equals( HttpMethod::newFromString( 'post' ) ) );
+	}
+
+	/**
+	 * @throws ExpectationFailedException
+	 * @throws InvalidArgumentException
+	 */
+	public function testGet() : void
+	{
+		$this->assertSame( 'GET', HttpMethod::get()->toString() );
+	}
+
+	/**
+	 * @throws ExpectationFailedException
+	 * @throws InvalidArgumentException
+	 */
+	public function testPost() : void
+	{
+		$this->assertSame( 'POST', HttpMethod::post()->toString() );
+	}
+}

3570f08305ebb44879f3e2ee4a8df6bc7ad044e7
Holger Woltersdorf <hw@hollo.me>
2020-04-14 14:19:45 +0200

    Remove config array constructor

--- src/Routing/RouteCollection.php
+++ src/Routing/RouteCollection.php
@@ -3,7 +3,6 @@
 namespace IceHawk\IceHawk\Routing;
 
 use Countable;
-use IceHawk\IceHawk\RequestHandlers\QueueRequestHandler;
 use InvalidArgumentException;
 use Iterator;
 use IteratorAggregate;
@@ -16,8 +15,6 @@ use function count;
  */
 final class RouteCollection implements Countable, IteratorAggregate
 {
-	private const DEFAULT_REQUEST_HANDLER_CLASS_NAME = QueueRequestHandler::class;
-
 	/** @var array<int, Route> */
 	private array $items;
 
@@ -29,30 +26,6 @@ final class RouteCollection implements Countable, IteratorAggregate
 	public static function new( Route ...$routes ) : self
 	{
 		return new self( ...$routes );
-	}
-
-	/**
-	 * @param array<string, array<string, mixed>> $configArray
-	 *
-	 * @return RouteCollection
-	 * @throws InvalidArgumentException
-	 */
-	public static function fromConfigArray( array $configArray ) : self
-	{
-		$collection = new self();
-		foreach ( $configArray as $routePattern => $routeInfo )
-		{
-			$collection->add(
-				Route::newFromStrings(
-					(string)($routeInfo['method'] ?? 'GET'),
-					(string)$routePattern,
-					(string)($routeInfo['handler'] ?? self::DEFAULT_REQUEST_HANDLER_CLASS_NAME),
-					...(array)($routeInfo['middlewares'] ?? [])
-				)
-			);
-		}
-
-		return $collection;
 	}
 
 	public function add( Route $route, Route ...$routes ) : void

44d63ffe1da136623e63f6b82a3e5f3317cb26e1
Holger Woltersdorf <hw@hollo.me>
2020-04-17 14:30:30 +0200

    Add http status type incl. tests

---
+++ tests/Unit/Types/HttpStatusTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace IceHawk\IceHawk\Tests\Unit\Types;
+
+use IceHawk\IceHawk\Types\HttpStatus;
+use InvalidArgumentException;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+use function fclose;
+use function fgetcsv;
+
+final class HttpStatusTest extends TestCase
+{
+	/**
+	 * @throws InvalidArgumentException
+	 * @throws ExpectationFailedException
+	 */
+	public function testFromCode() : void
+	{
+		$handle = fopen( __DIR__ . '/_files/http-status-codes.csv', 'rb' );
+		while ( $line = fgetcsv( $handle ) )
+		{
+			if ( 'Value' === $line[0] )
+			{
+				continue;
+			}
+
+			$httpStatus = HttpStatus::fromCode( (int)$line[0] );
+
+			$this->assertSame( (int)$line[0], $httpStatus->getCode() );
+			$this->assertSame( (string)$line[1], $httpStatus->getPhrase() );
+			$this->assertSame( $line[0] . ' ' . $line[1], $httpStatus->toString() );
+		}
+
+		fclose( $handle );
+	}
+}
